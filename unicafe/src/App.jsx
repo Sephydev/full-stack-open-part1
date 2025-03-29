e00a9e395a89f335d8c1e1d5f1e3365578a64ca5
Sephydev <nicolas.bussiere@outlook.fr>
2025-03-29 14:39:45 +0100

    Refactor Statistic line into component

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -8,18 +8,24 @@ const Button = (props) => {
   )
 }
 
+const StatisticLine = (props) => {
+  return (
+    <p>{props.text}: {props.statistic}{props.percent ? '%' : ''}</p>
+  )
+}
+
 const Statistics = (props) => {
   return (
     <>
       <h2>Statistics</h2>
       {props.all > 0 ?
         <>
-          <p>Good: {props.good}</p>
-          <p>Neutral: {props.neutral}</p>
-          <p>Bad: {props.bad}</p>
-          <p>All: {props.all}</p>
-          <p>Average: {props.average}</p>
-          <p>Positive: {props.positive}%</p>
+          <StatisticLine text={'Good'} statistic={props.good} />
+          <StatisticLine text={'Neutral'} statistic={props.neutral} />
+          <StatisticLine text={'Bad'} statistic={props.bad} />
+          <StatisticLine text={'All'} statistic={props.all} />
+          <StatisticLine text={'Average'} statistic={props.average} />
+          <StatisticLine text={'Positive'} statistic={props.positive} percent={true} />
         </>
         :
         <p>No feedback given</p>
@@ -27,6 +33,7 @@ const Statistics = (props) => {
     </>
   )
 }
+
 
 const App = () => {
   // save clicks of each button to its own state
