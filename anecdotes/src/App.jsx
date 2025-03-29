3c6640f56a2f5f978419855cfeb9ef5db07824ff
Sephydev <nicolas.bussiere@outlook.fr>
2025-03-29 15:29:40 +0100

    Add starting code

--- anecdotes/src/App.jsx
+++ anecdotes/src/App.jsx
@@ -1,34 +1,23 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const anecdotes = [
+    'Programming today is a race between software engineers striving to build bigger and better idiot-proof programs, and the Universe trying to produce bigger and better idiots. SO far, the Universe is winning.',
+    'If builders built buildings the way programmers wrote programs, then the first woodpecker that came along would destroy civilization.',
+    'I think it\'s a new feature. Don\'t tell anyone it was an accident.',
+    'Theory is when you know something, but it doesn\'t work. Practice is when something works, but you don\'t know why. Programmers combine theory and practice: Nothing works and they don\'t know why.',
+    'The goal of Computer Science is to build something that will last until we\'ve finished building it.',
+    'The most likely way for the world to be destroyed, most experts agree, is by accident. That\'s where we come in; we\'re computer professionals. We cause accidents.',
+    'Rule of Optimization: Rule 1: don\'t do it. Rule 2 (for experts only): Don\'t do it yet.',
+    'If the automobile had followed the same development cycle as the computer, a Rolls-Royce would today cost $100, get a million miles per gallon, and explode once a year, killing everyone inside.'
+  ]
+
+  const [selected, setSelected] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      {anecdotes[selected]}
+    </div>
   )
 }
 
