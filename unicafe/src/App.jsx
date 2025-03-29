3bc0af480a4d9a040ad12abb9beaf59d5dfb0d63
Sephydev <nicolas.bussiere@outlook.fr>
2025-03-29 14:27:33 +0100

    Render another text if no vote

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -4,12 +4,18 @@ const Statistics = (props) => {
   return (
     <>
       <h2>Statistics</h2>
-      <p>Good: {props.good}</p>
-      <p>Neutral: {props.neutral}</p>
-      <p>Bad: {props.bad}</p>
-      <p>All: {props.all}</p>
-      <p>Average: {props.average}</p>
-      <p>Positive: {props.positive}%</p>
+      {props.all > 0 ?
+        <>
+          <p>Good: {props.good}</p>
+          <p>Neutral: {props.neutral}</p>
+          <p>Bad: {props.bad}</p>
+          <p>All: {props.all}</p>
+          <p>Average: {props.average}</p>
+          <p>Positive: {props.positive}%</p>
+        </>
+        :
+        <p>No feedback given</p>
+      }
     </>
   )
 }
