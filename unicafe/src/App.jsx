cb0ba13081e6f66326293d1060d5adbe4ddefec7
Sephydev <nicolas.bussiere@outlook.fr>
2025-03-29 12:50:58 +0100

    Display total number of vote

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -5,17 +5,21 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
 
   const handleGood = () => {
     setGood(good + 1)
+    setAll(all + 1)
   }
 
   const handleNeutral = () => {
     setNeutral(neutral + 1)
+    setAll(all + 1)
   }
 
   const handleBad = () => {
     setBad(bad + 1)
+    setAll(all + 1)
   }
 
   return (
@@ -28,6 +32,7 @@ const App = () => {
       <p>Good: {good}</p>
       <p>Neutral: {neutral}</p>
       <p>Bad: {bad}</p>
+      <p>All: {all}</p>
     </div>
   )
 }
